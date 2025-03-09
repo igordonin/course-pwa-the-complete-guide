5e63a33fec7b5892a409704cfba036e1e54eb6f0
Igor Donin <igordonin@gmail.com>
2025-03-09 17:54:57 -0300

    create or open an existing cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,19 @@
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+
+  // in this case, we need to wait for the initial caching operation to
+  // finish to avoid other events from trying to access the cache when
+  // it's not ready yet.
+  event.waitUntil(
+    // this name is arbitrary
+    caches.open('static').then(
+      // we receive a reference to the cache
+      function (cache) {
+        console.log('[Service Worker] Precaching App Shell...', event);
+        cache.add('/src/js/app.js');
+      }
+    )
+  );
 });
 
 self.addEventListener('activate', function (event) {
